12910464b27f18229632c7af18db886684e32bfb
Pinal-Bhatt-Live <Pinal-Bhatt@live.com>
2014-02-16 09:00:37 -0500

    2.0.1

---
+++ jQuery.BootStrap.FontAwesome/Make.bat
@@ -0,0 +1,1 @@
+msbuild Nuget.MSBuild /p:FontAwesomeVersion=4.0.3 /p:bootstrapVersion=3.1.1 /p:jQueryVersion=2.1.0 /p:MajorVersion=2 /p:MinorVersion=0 /p:BuildNumber=1
--- jQuery.BootStrap.FontAwesome/Nuget/Make.bat
+++
@@ -1,1 +0,0 @@
-msbuild Nuget.MSBuild /p:FontAwesomeVersion=4.0.3 /p:bootstrapVersion=3.1.0 /p:jQueryVersion=2.1.0 /p:MajorVersion=2 /p:MinorVersion=0 /p:BuildNumber=0
