56bbdf31d1f6c522de77a1117f07ad15ef891709
PinalBhattOutlook <Pinal.Bhatt@outlook.com>
2014-03-23 09:12:27 -0400

    videogular startup

---
+++ videogular/publish.bat
@@ -0,0 +1,16 @@
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Amelia\Twitter.Bootstrap.Bootswatch.Amelia.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Cerulean\Twitter.Bootstrap.Bootswatch.Cerulean.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Cosmo\Twitter.Bootstrap.Bootswatch.Cosmo.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Cyborg\Twitter.Bootstrap.Bootswatch.Cyborg.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Flatly\Twitter.Bootstrap.Bootswatch.Flatly.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Journal\Twitter.Bootstrap.Bootswatch.Journal.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Readable\Twitter.Bootstrap.Bootswatch.Readable.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Simplex\Twitter.Bootstrap.Bootswatch.Simplex.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Slate\Twitter.Bootstrap.Bootswatch.Slate.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Spacelab\Twitter.Bootstrap.Bootswatch.Spacelab.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\United\Twitter.Bootstrap.Bootswatch.United.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Yeti\Twitter.Bootstrap.Bootswatch.Yeti.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Lumen\Twitter.Bootstrap.Bootswatch.Lumen.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Superhero\Twitter.Bootstrap.Bootswatch.Superhero.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Shamrock\Twitter.Bootstrap.Bootswatch.Shamrock.3.1.1.1.nupkg -source https://www.nuget.org/
+..\NugetExe\nuget.exe push D:\Pinal.Bhatt\Git\GitHub\Nugets\Twitter.Bootstrap.Bootswatch\Packages\3.1.1.1\Darkly\Twitter.Bootstrap.Bootswatch.Darkly.3.1.1.1.nupkg -source https://www.nuget.org/
